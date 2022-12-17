a9945ea870981056c37365a55ca8f05529880c3d
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-12-16 17:37:55 -0800

    Initial Commit (Stole my own code from CS290)

---
+++ script.js
@@ -0,0 +1,20 @@
+//js stuff
+
+/*
+function createParagraph() {
+  const para = document.createElement('p');
+  para.textContent = 'You clicked the button! :D';
+  document.body.appendChild(para);
+
+}
+
+function createImage() {
+
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons) {
+  button.addEventListener('click', createParagraph);
+}
+*/
