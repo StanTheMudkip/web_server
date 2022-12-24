3689821fd29dc17dd8ce2fbb36fd3d6a1886535a
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-12-24 11:10:27 -0800

    Experimenting with profile card popout animation
    Got it basically working, but not exactly how I would like.

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-//js stuff
+const card = document.querySelector(".pfp");
+const container = document.querySelector(".profile-card");
 
-/*
-function createParagraph() {
-  const para = document.createElement('p');
-  para.textContent = 'You clicked the button! :D';
-  document.body.appendChild(para);
-
-}
-
-function createImage() {
-
-}
-
-const buttons = document.querySelectorAll('button');
-
-for (const button of buttons) {
-  button.addEventListener('click', createParagraph);
-}
-*/
+//add event listener for mouse movement that rotate the picture based one mouse movement.
+container.addEventListener("mousemove", (e) => {
+  //Take the windows inner width and the mouse movement change and divide it by 25.
+  let x = (window.innerWidth / 2 - e.pageX) / 25;
+  let y = (window.innerHeight / 2 - e.pageY) / 25;
+  //Change the rotation based on the degree of the new coordinates.
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
